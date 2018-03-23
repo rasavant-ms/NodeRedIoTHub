e835f54cd1861aee6b30688a03e7e295e299f9ee
Rajasa Savant <rasavant@microsoft.com>
2018-03-23 15:37:25 -0700

    Uncommented tests to verify CircleCI integration

--- test/azure-iot-hub_spec.js
+++ test/azure-iot-hub_spec.js
@@ -9,42 +9,42 @@ describe('AzureIoTHubNode Node', function () {
     helper.unload();
   });
 
-  // it('azureiothub should be loaded', function (done) {
-  //   var flow = [{ id: "n1", type: "azureiothub", name: "azureiothub", credentials: {hostname: "hostname"} }];
-  //   helper.load(azureiothub, flow, function () {
-  //     var n1 = helper.getNode("n1");
-  //     n1.should.have.property('name', 'azureiothub');
-  //     n1.should.have.property('credentials', {});
-  //   });
-  //   done();
-  // });
+  it('azureiothub should be loaded', function (done) {
+    var flow = [{ id: "n1", type: "azureiothub", name: "azureiothub", credentials: {hostname: "hostname"} }];
+    helper.load(azureiothub, flow, function () {
+      var n1 = helper.getNode("n1");
+      n1.should.have.property('name', 'azureiothub');
+      n1.should.have.property('credentials', {});
+    });
+    done();
+  });
 
-  // it('azureiothubregistry should be loaded', function (done) {
-  //   var flow = [{ id: "n2", type: "azureiothubregistry", name: "azureiothubregistry" }];
-  //   helper.load(azureiothub, flow, function () {
-  //     var n2 = helper.getNode("n2");
-  //     n2.should.have.property('name', 'azureiothubregistry');
-  //   });
-  //   done();
-  // });
+  it('azureiothubregistry should be loaded', function (done) {
+    var flow = [{ id: "n2", type: "azureiothubregistry", name: "azureiothubregistry" }];
+    helper.load(azureiothub, flow, function () {
+      var n2 = helper.getNode("n2");
+      n2.should.have.property('name', 'azureiothubregistry');
+    });
+    done();
+  });
 
-  // it('azureiothubreceiver should be loaded', function (done) {
-  //   var flow = [{ id: "n3", type: "azureiothubreceiver", name: "azureiothubreceiver" }];
-  //   helper.load(azureiothub, flow, function () {
-  //     var n3 = helper.getNode("n3");
-  //     n3.should.have.property('name', 'azureiothubreceiver');
-  //   });
-  //   done();
-  // });
+  it('azureiothubreceiver should be loaded', function (done) {
+    var flow = [{ id: "n3", type: "azureiothubreceiver", name: "azureiothubreceiver" }];
+    helper.load(azureiothub, flow, function () {
+      var n3 = helper.getNode("n3");
+      n3.should.have.property('name', 'azureiothubreceiver');
+    });
+    done();
+  });
 
-  // it('azureiothubdevicetwin should be loaded', function (done) {
-  //   var flow = [{ id: "n4", type: "azureiothubdevicetwin", name: "azureiothubdevicetwin" }];
-  //   helper.load(azureiothub, flow, function () {
-  //     var n4 = helper.getNode("n4");
-  //     n4.should.have.property('name', 'azureiothubdevicetwin');
-  //   });
-  //   done();
-  // });
+  it('azureiothubdevicetwin should be loaded', function (done) {
+    var flow = [{ id: "n4", type: "azureiothubdevicetwin", name: "azureiothubdevicetwin" }];
+    helper.load(azureiothub, flow, function () {
+      var n4 = helper.getNode("n4");
+      n4.should.have.property('name', 'azureiothubdevicetwin');
+    });
+    done();
+  });
 
 
   it('azureiothub should receive input', function (done) {
@@ -68,9 +68,9 @@ describe('AzureIoTHubNode Node', function () {
         msg.should.have.property('credentials', '{hostname:"foo"}');
         
         azIoTNode.receive({protocol:"amqp", credentials: "{hostname:'hostname'}"});
-        done();
+        //done();
       });
-      //done();
+      done();
       
      
     });
